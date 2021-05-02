13f185312e257a5692e4e07672c42487676de8d5
Christopher Yeager <46506442+yeagercmbpd@users.noreply.github.com>
2021-05-02 13:43:56 -0400

    Update Assoc.py

--- Assoc.py
+++ Assoc.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import qgrid
+
 import os
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
